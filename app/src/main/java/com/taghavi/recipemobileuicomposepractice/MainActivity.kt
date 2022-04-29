935b3c80889c8e3b2a527210455864934bcb7b7a
EhsanTgv <EhsanTgv@gmail.com>
2022-04-29 10:15:22 +0300

    Implement ParallaxToolbar in the main page

--- app/src/main/java/com/taghavi/recipemobileuicomposepractice/MainActivity.kt
+++ app/src/main/java/com/taghavi/recipemobileuicomposepractice/MainActivity.kt
@@ -3,13 +3,20 @@ package com.taghavi.recipemobileuicomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.taghavi.recipemobileuicomposepractice.ui.theme.RecipeMobileUIComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +24,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RecipeMobileUIComposePracticeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainFragment(strawberryCakeRecipe)
                 }
             }
         }
@@ -30,14 +36,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainFragment(recipe: Recipe) {
+    Box {
+//        Content(recipe)
+        ParallaxToolbar(recipe)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ParallaxToolbar(recipe: Recipe) {
+    val imageHeight = AppBarExpendedHeight - AppBarCollapsedHeight
+    TopAppBar(
+        contentPadding = PaddingValues(),
+        backgroundColor = Color.White,
+        modifier = Modifier.height(AppBarExpendedHeight),
+    ) {
+        Column {
+            Box(Modifier.height(imageHeight)) {
+                Image(
+                    painter = painterResource(id = R.drawable.strawberry_pie_1),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colorStops = arrayOf(
+                                    Pair(0.4f, Color.Transparent),
+                                    Pair(1f, Color.White),
+                                )
+                            )
+                        )
+                )
+            }
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .height(AppBarCollapsedHeight)
+            ) {
+                Text(
+                    text = recipe.title,
+                    fontSize = 26.sp,
+                    fontWeight = Bold,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 380, heightDp = 720)
 @Composable
 fun DefaultPreview() {
     RecipeMobileUIComposePracticeTheme {
-        Greeting("Android")
+        MainFragment(strawberryCakeRecipe)
     }
 }
