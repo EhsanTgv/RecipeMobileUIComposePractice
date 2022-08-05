00ba7dddff2910d70a42a4cc64e26d4ca151f075
EhsanTgv <EhsanTgv@gmail.com>
2022-08-05 10:43:43 +0300

    Implement Basic Info & Description & Serving calculator

--- app/src/main/java/com/taghavi/recipemobileuicomposepractice/MainActivity.kt
+++ app/src/main/java/com/taghavi/recipemobileuicomposepractice/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -18,12 +19,12 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight.Companion.Medium
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.insets.statusBarsPadding
-import com.taghavi.recipemobileuicomposepractice.ui.theme.RecipeMobileUIComposePracticeTheme
-import com.taghavi.recipemobileuicomposepractice.ui.theme.Shapes
+import com.taghavi.recipemobileuicomposepractice.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +45,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainFragment(recipe: Recipe) {
     Box {
-//        Content(recipe)
+        Content(recipe)
         ParallaxToolbar(recipe)
     }
 }
@@ -54,7 +55,7 @@ fun ParallaxToolbar(recipe: Recipe) {
     val imageHeight = AppBarExpendedHeight - AppBarCollapsedHeight
     TopAppBar(
         contentPadding = PaddingValues(),
-        backgroundColor = Color.White,
+        backgroundColor = White,
         modifier = Modifier.height(AppBarExpendedHeight),
     ) {
         Column {
@@ -71,8 +72,8 @@ fun ParallaxToolbar(recipe: Recipe) {
                         .background(
                             Brush.verticalGradient(
                                 colorStops = arrayOf(
-                                    Pair(0.4f, Color.Transparent),
-                                    Pair(1f, Color.White),
+                                    Pair(0.4f, Transparent),
+                                    Pair(1f, White),
                                 )
                             )
                         )
@@ -89,7 +90,7 @@ fun ParallaxToolbar(recipe: Recipe) {
                         fontWeight = FontWeight.Medium,
                         modifier = Modifier
                             .clip(Shapes.small)
-                            .background(Color.LightGray)
+                            .background(LightGray)
                             .padding(horizontal = 6.dp, vertical = 6.dp)
                     )
                 }
@@ -126,7 +127,7 @@ fun ParallaxToolbar(recipe: Recipe) {
 @Composable
 fun CircularButton(
     @DrawableRes iconResource: Int,
-    color: Color = Color.Gray,
+    color: Color = Gray,
     elevation: ButtonElevation? = ButtonDefaults.elevation(),
     onClick: () -> Unit = {}
 ) {
@@ -134,13 +135,77 @@ fun CircularButton(
         onClick = onClick,
         contentPadding = PaddingValues(),
         shape = Shapes.small,
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White, contentColor = color),
+        colors = ButtonDefaults.buttonColors(backgroundColor = White, contentColor = color),
         elevation = elevation,
         modifier = Modifier
             .width(38.dp)
             .height(38.dp)
     ) {
         Icon(painter = painterResource(id = iconResource), null)
+    }
+}
+
+@Composable
+fun Content(recipe: Recipe) {
+    LazyColumn(contentPadding = PaddingValues(top = AppBarExpendedHeight)) {
+        item {
+            BasicInfo(recipe)
+            Description(recipe)
+            ServingCalculator()
+        }
+    }
+}
+
+@Composable
+fun ServingCalculator() {
+    var value by remember { mutableStateOf(6) }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .clip(Shapes.medium)
+            .background(LightGray)
+            .padding(horizontal = 16.dp)
+    ) {
+        Text(text = "Serving", Modifier.weight(1f), fontWeight = Medium)
+        CircularButton(iconResource = R.drawable.ic_minus, elevation = null, onClick = { value-- })
+        Text(text = "$value", Modifier.padding(16.dp), fontWeight = Medium)
+        CircularButton(iconResource = R.drawable.ic_plus, elevation = null, onClick = { value++ })
+    }
+}
+
+@Composable
+fun Description(recipe: Recipe) {
+    Text(
+        text = recipe.description,
+        fontWeight = Medium,
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)
+    )
+}
+
+@Composable
+fun BasicInfo(recipe: Recipe) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    ) {
+        InfoColumn(R.drawable.ic_clock, recipe.cookingTime)
+        InfoColumn(R.drawable.ic_flame, recipe.energy)
+        InfoColumn(R.drawable.ic_star, recipe.rating)
+    }
+}
+
+@Composable
+fun InfoColumn(@DrawableRes iconResource: Int, text: String) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Icon(
+            painter = painterResource(id = iconResource),
+            contentDescription = null,
+            tint = Pink
+        )
+        Text(text = text, fontWeight = Bold)
     }
 }
 
